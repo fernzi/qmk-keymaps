ac95f9245ac987a9e71085ffd11f95c5784125f1
Fern Zapata <fern@zapata.cc>
2023-09-09 03:00:42 -0600

    Docs: Use a terser license header
    Also, signing my commits here now. Feels like a good habit in general.

--- src/keyboards/xiudi/xd75/keymaps/fernzi/rules.mk
+++ src/keyboards/xiudi/xd75/keymaps/fernzi/rules.mk
@@ -1,8 +1,7 @@
 # Copyright © 2021-2023 Fern Zapata
-# This program is subject to the terms of the GNU GPL, either
-# version 3 of the License or (at your option) any later version.
-# You should have received a copy of the License along with
-# this program. If not, see <https://www.gnu.org/licenses/>.
+# This program is subject to the terms of the GNU GPL, version 3
+# or, at your option, any later version. If a copy of it was not
+# included with this file, see https://www.gnu.org/licenses/.
 
 RGBLIGHT_ENABLE = no
 MOUSEKEY_ENABLE = no
