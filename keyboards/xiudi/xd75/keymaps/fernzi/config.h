c83e7e9a27d0f804df0e3e8f92a626ce1b677581
Fern Zapata <fern@zapata.cc>
2025-09-06 02:47:54 -0600

    Disable NKRO
    Enabled it just because, really. Gonna test if 6KRO is enough.

--- keyboards/xiudi/xd75/keymaps/fernzi/config.h
+++ keyboards/xiudi/xd75/keymaps/fernzi/config.h
@@ -1,10 +1,9 @@
-/* Copyright © 2021-2023 Fern Zapata
- * This program is subject to the terms of the GNU GPL, version 3
- * or, at your option, any later version. If a copy of it was not
- * included with this file, see https://www.gnu.org/licenses/. */
+/* Copyright © 2021-2025 Fern Zapata
+ * This file is under the terms of the GNU GPL version 3, or (at your
+ * option) any later version. If you didn't receive a copy of the GPL
+ * along with this file, see <https://www.gnu.org/licenses/>. */
 
 #pragma once
 
-#define FORCE_NKRO
 #define JOYSTICK_AXIS_COUNT 6
 #define JOYSTICK_BUTTON_COUNT 13
