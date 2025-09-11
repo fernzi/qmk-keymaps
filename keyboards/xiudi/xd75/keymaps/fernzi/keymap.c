0002809c9edb0533c6e7ebd4b5743c8abab8304d
Fern Zapata <fern@zapata.cc>
2025-09-11 07:20:25 -0600

    Don't custom-handle base layout switching

--- keyboards/xiudi/xd75/keymaps/fernzi/keymap.c
+++ keyboards/xiudi/xd75/keymaps/fernzi/keymap.c
@@ -8,21 +8,20 @@
 
 enum my_layer
 {
-  _DV,
-  _QW,
+  LAYER_DVORAK,
+  LAYER_QWERTY,
 #if JOYSTICK_ENABLE
-  _HB,
+  LAYER_HITBOX,
 #endif
-  _L0,
-  _R0,
-  _F0,
-  _CF,
+  LAYER_LOWER,
+  LAYER_UPPER,
+  LAYER_FUNCT,
+  LAYER_ADJUST,
 };
 
-enum xd75_keycodes
+enum my_keycode
 {
-  DVORAK = SAFE_RANGE,
-  QWERTY,
+  MKC_P00 = SAFE_RANGE,
 #if JOYSTICK_ENABLE
   HB_MOD,
   HB_SWAP,
@@ -31,20 +30,26 @@ enum xd75_keycodes
   HB_UP,
   HB_RGHT,
 #endif
-  MKC_P00,
 };
 
 enum my_layer_keycode
 {
-  MY_LOWR = LT(_L0, KC_SPC),
-  MY_UPPR = LT(_R0, KC_SPC),
-  MY_FUNC = MO(_F0),
+  MY_DVRK = PDF(LAYER_DVORAK),
+  MY_QRTY = PDF(LAYER_QWERTY),
+#if JOYSTICK_ENABLE
+  MY_HBOX = TG(LAYER_HITBOX),
+#else
+  MY_HBOX = _______,
+#endif
+
+  MY_LOWR = LT(LAYER_LOWER, KC_SPC),
+  MY_UPPR = LT(LAYER_UPPER, KC_SPC),
+  MY_FUNC = MO(LAYER_FUNCT),
 };
 
 #if JOYSTICK_ENABLE
 enum my_hitbox_keycode
 {
-  HITBOX = TG(_HB),
   HB_BTA = JS_0,
   HB_BTB,
   HB_BTX,
@@ -70,12 +75,10 @@ joystick_config_t joystick_axes[JOYSTICK_AXIS_COUNT] = {
   [4] = JOYSTICK_AXIS_VIRTUAL,
   [5] = JOYSTICK_AXIS_VIRTUAL,
 };
-#else
-#define HITBOX XXXXXXX
 #endif // JOYSTICK_ENABLE
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_DV] = LAYOUT_ortho_5x15( // DVORAK
+  [LAYER_DVORAK] = LAYOUT_ortho_5x15(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, KC_P7,   KC_P8,   KC_P9,
     KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_DEL,  KC_P4,   KC_P5,   KC_P6,
     KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH, KC_P1,   KC_P2,   KC_P3,
@@ -83,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, MY_FUNC, KC_LGUI, KC_LALT, MY_LOWR, KC_SPC,  KC_SPC,  MY_UPPR, KC_RALT, KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
-  [_QW] = LAYOUT_ortho_5x15( // QWERTY
+  [LAYER_QWERTY] = LAYOUT_ortho_5x15(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, KC_P7,   KC_P8,   KC_P9,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  KC_P4,   KC_P5,   KC_P6,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_P1,   KC_P2,   KC_P3,
@@ -92,8 +95,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
 #if JOYSTICK_ENABLE
-  [_HB] = LAYOUT_ortho_5x15( // HITBOX
-    HITBOX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, HB_BTSL, HB_BTHM, HB_BTST,
+  [LAYER_HITBOX] = LAYOUT_ortho_5x15(
+    MY_HBOX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, HB_BTSL, HB_BTHM, HB_BTST,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, HB_BTL1, HB_BTR1, HB_BTL2, HB_BTR2,
     HB_MOD,  HB_LEFT, HB_DOWN, HB_RGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, HB_BTA,  HB_BTB,  HB_BTX,  HB_BTY,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -101,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 #endif
 
-  [_L0] = LAYOUT_ortho_5x15( // LOWER
+  [LAYER_LOWER] = LAYOUT_ortho_5x15(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, KC_PSLS, KC_PAST, KC_PMNS,
     _______, _______, KC_LCBR, KC_RCBR, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, KC_INS,  KC_NUM,  _______, KC_PPLS,
     KC_DEL,  _______, _______, KC_PLUS, KC_UNDS, _______, _______, KC_UNDS, KC_PLUS, _______, _______, KC_PIPE, _______, _______, KC_PENT,
@@ -109,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
   ),
 
-  [_R0] = LAYOUT_ortho_5x15( // RAISE
+  [LAYER_UPPER] = LAYOUT_ortho_5x15(
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, KC_PSLS, KC_PAST, KC_PMNS,
     _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, KC_LCBR, KC_RCBR, _______, KC_INS,  KC_NUM,  _______, KC_PPLS,
     KC_DEL,  _______, _______, KC_EQL,  KC_MINS, _______, _______, KC_MINS, KC_EQL,  _______, _______, KC_BSLS, _______, _______, KC_PENT,
@@ -117,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
   ),
 
-  [_F0] = LAYOUT_ortho_5x15( // FUNCTION
+  [LAYER_FUNCT] = LAYOUT_ortho_5x15(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
     KC_PSCR, _______, KC_LPRN, KC_RPRN, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, _______,
@@ -125,8 +128,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT
   ),
 
-  [_CF] = LAYOUT_ortho_5x15( // CONFIGRURE
-    _______, DVORAK,  QWERTY,  HITBOX,  _______, _______, _______, _______, _______, _______, _______, _______, DB_TOGG, EE_CLR,  QK_BOOT,
+  [LAYER_ADJUST] = LAYOUT_ortho_5x15(
+    _______, MY_DVRK, MY_QRTY, MY_HBOX, _______, _______, _______, _______, _______, _______, _______, _______, DB_TOGG, EE_CLR,  QK_BOOT,
     _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -136,7 +139,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state)
 {
-  return update_tri_layer_state(state, _L0, _R0, _CF);
+  return update_tri_layer_state(
+    state, LAYER_LOWER, LAYER_UPPER, LAYER_ADJUST);
 }
 
 #if JOYSTICK_ENABLE
@@ -153,20 +157,11 @@ static int8_t to_axis(int8_t x)
 {
   return MY_CMP(x, 0) * HB_JHI >> axes.mod;
 }
-
 #endif // JOYSTICK_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record)
 {
   switch (keycode) {
-    case DVORAK:
-      if (record->event.pressed)
-        set_single_persistent_default_layer(_DV);
-      return false;
-    case QWERTY:
-      if (record->event.pressed)
-        set_single_persistent_default_layer(_QW);
-      return false;
 #if JOYSTICK_ENABLE
     case HB_MOD:
       axes.mod = record->event.pressed;
